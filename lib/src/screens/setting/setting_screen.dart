ffd5828b17a848122413e546608151d1f1001397
Friox <ljykdltmdgns624@gmail.com>
2024-06-05 19:40:29 +0900

    #5 AppBar 및 페이지 헤더 수정, 폰트 추가

--- lib/src/screens/setting/setting_screen.dart
+++ lib/src/screens/setting/setting_screen.dart
@@ -1,3 +1,4 @@
+import 'package:app/constants/layout_constants.dart';
 import 'package:app/src/widgets/screen_header.dart';
 import 'package:flutter/material.dart';
 
@@ -11,11 +12,24 @@ class SettingScreen extends StatefulWidget {
 class _SettingScreenState extends State<SettingScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: [
-          ScreenHeader(title: 'Setting', showProfile: false,)
-        ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: LayoutConstants.headerPaddingVertical),
+        child: Column(
+          children: [
+            ScreenHeader(
+              title: 'Setting',
+              subTitle: '부제목, 사용안하면 제거가능',
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: LayoutConstants.containerPadding),
+              child: Column(
+                children: [],
+              ),
+            )
+            
+          ],
+        ),
       ),
     );
   }
--- lib/src/widgets/screen_header.dart
+++ lib/src/widgets/screen_header.dart
@@ -1,10 +1,13 @@
+import 'package:app/constants/layout_constants.dart';
 import 'package:flutter/material.dart';
 
 class ScreenHeader extends StatefulWidget {
-  const ScreenHeader({super.key, required this.title, required this.showProfile});
+  const ScreenHeader({super.key, required this.title, this.subTitle, this.onClick, this.icon});
 
   final String title;
-  final bool showProfile;
+  final String? subTitle;
+  final VoidCallback? onClick;
+  final Icon? icon;
 
   @override
   State<ScreenHeader> createState() => _ScreenHeaderState();
@@ -15,15 +18,43 @@ class _ScreenHeaderState extends State<ScreenHeader> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.transparent,
-      padding: const EdgeInsets.only(left: 32, top: 24, right: 32, bottom: 16),
+      padding: const EdgeInsets.only(
+        left: LayoutConstants.containerPadding,
+        top: LayoutConstants.headerPaddingVertical,
+        right: LayoutConstants.headerPaddingRight,
+        bottom: LayoutConstants.headerPaddingVertical
+      ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text(
-            widget.title,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 6),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.title,
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontVariations: LayoutConstants.fontWeightBold
+                  ),
+                ),
+                if (widget.subTitle != null) Text(
+                  widget.subTitle!,
+                  style: TextStyle(
+                    fontSize: 16,
+                  )
+                )
+              ],
+            ),
           ),
-          if (widget.showProfile) Icon(Icons.account_circle_rounded, size: 36,)
+          if (widget.onClick != null && widget.icon != null) Padding(
+            padding: const EdgeInsets.only(right: 0),
+            child: IconButton(
+              onPressed: widget.onClick,
+              icon: widget.icon!,
+            ),
+          )
         ],
       ),
     );
