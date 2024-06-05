ca1b446b7ef844681e0df7d04249931c55db755a
Friox <ljykdltmdgns624@gmail.com>
2024-06-05 23:07:50 +0900

    Container Card height Support

--- lib/widgets/container_card.dart
+++ lib/widgets/container_card.dart
@@ -2,12 +2,20 @@ import 'package:app/constants/layout_constants.dart';
 import 'package:flutter/material.dart';
 
 class ContainerCard extends StatefulWidget {
-  const ContainerCard({super.key, this.title, this.subTitle, this.onClick, required this.child});
+  const ContainerCard({
+    super.key,
+    this.title,
+    this.subTitle,
+    this.onClick,
+    required this.child,
+    this.height
+  });
 
   final String? title;
   final String? subTitle;
   final VoidCallback? onClick;
   final Widget child;
+  final double? height;
 
   @override
   State<ContainerCard> createState() => _ContainerCardState();
@@ -17,6 +25,7 @@ class _ContainerCardState extends State<ContainerCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: widget.height,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(LayoutConstants.containerCornerRadius),
         border: Border.all(
