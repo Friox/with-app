d6c47ab154c24ffbb69b18f6b1ab8163edabe8d4
Hanyeojun <hyj78396@naver.com>
2024-06-05 17:16:47 +0900

    Q&A page elements(#2)

--- lib/src/screens/question/question_screen.dart
+++ lib/src/screens/question/question_screen.dart
@@ -1,5 +1,6 @@
 import 'package:app/src/widgets/screen_header.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -21,12 +22,104 @@ class _QuestionScreenState extends State<QuestionScreen> {
           Align(
             alignment: Alignment.topCenter,
             child: Container(
-              width: 330,
-              height: 380,
-              decoration: BoxDecoration(
-                  border: Border.all(color: const Color(0xFFF1C40F), width: 1),
-                  borderRadius: BorderRadius.circular(16)),
-            ),
+                width: 330,
+                height: 380,
+                decoration: BoxDecoration(
+                    border:
+                        Border.all(color: const Color(0xFFF1C40F), width: 1),
+                    borderRadius: BorderRadius.circular(16)),
+                padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+                child: Column(
+                  children: [
+                    Row(children: [
+                      Text(
+                        '질문',
+                        style:
+                            TextStyle(color: Color(0xFF666666), fontSize: 18),
+                      ),
+                      Spacer(
+                        flex: 1,
+                      ),
+                      Icon(Icons.arrow_forward_ios, color: Color(0xFF666666))
+                    ]),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.radio_button_unchecked),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Test question 1',
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 15),
+                            ),
+                            Text(
+                              'who asked',
+                              style: TextStyle(
+                                color: Color(0xFF666666),
+                                fontSize: 11,
+                              ),
+                            )
+                          ],
+                        ),
+                        Spacer(
+                          flex: 1,
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            '빠른 답변',
+                            style: TextStyle(
+                                color: Color(0xFF666666), fontSize: 11),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.radio_button_unchecked),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Test question 2',
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 15),
+                            ),
+                            Text(
+                              'who asked',
+                              style: TextStyle(
+                                  color: Color(0xFF666666), fontSize: 11),
+                            )
+                          ],
+                        ),
+                        Spacer(
+                          flex: 1,
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            '빠른 답변',
+                            style: TextStyle(
+                                color: Color(0xFF666666), fontSize: 11),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                )),
           ),
           Spacer(flex: 1),
           ElevatedButton(
@@ -41,9 +134,12 @@ class _QuestionScreenState extends State<QuestionScreen> {
             child: Text('질문 답변하러 가기',
                 style: TextStyle(color: Colors.black, fontSize: 15)),
           ),
+          SizedBox(
+            height: 2,
+          ),
           Text(
-            '나에게 온 질문에 답장해보아요',
-            style: TextStyle(color: Color.fromARGB(150, 110, 108, 108)),
+            '나에게 온 질문에 답변해보아요',
+            style: TextStyle(color: Color(0xFF666666)),
           ),
           Spacer(flex: 1),
           ElevatedButton(
@@ -60,9 +156,12 @@ class _QuestionScreenState extends State<QuestionScreen> {
               style: TextStyle(color: Colors.black, fontSize: 15),
             ),
           ),
+          SizedBox(
+            height: 2,
+          ),
           Text(
             '궁금한 점을 질문해보아요',
-            style: TextStyle(color: Color.fromARGB(150, 110, 108, 108)),
+            style: TextStyle(color: Color(0xFF666666)),
           ),
           Spacer(flex: 1),
         ],
