f3d0bba1a56577d64627b7497cc0ab14e6fbee98
Hanyeojun <hyj78396@naver.com>
2024-06-08 23:21:25 +0900

    페이지 재구성

--- lib/screens/question/question_screen.dart
+++ lib/screens/question/question_screen.dart
@@ -11,144 +11,276 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
+  final List<String> dropdownList = ['전체', '백승준', '이승훈', '이주환', '최영준', '한여준'];
+  String selectedval = '전체';
+
+  final List<String> searchList = ['전체', '백승준', '이승훈', '이주환', '최영준', '한여준'];
+  String searchval = '전체';
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
-        padding: const EdgeInsets.only(bottom: LayoutConstants.headerPaddingVertical),
+        padding: const EdgeInsets.only(
+            bottom: LayoutConstants.headerPaddingVertical),
         child: Column(
           children: [
             ScreenHeader(
               title: 'Q&A',
-              subTitle: '부제목, 사용안하면 제거가능',
+              subTitle: '궁금한 것을 묻고 답해보아요',
             ),
             Container(
-              padding: EdgeInsets.symmetric(horizontal: LayoutConstants.containerPadding),
+              padding: EdgeInsets.symmetric(
+                  horizontal: LayoutConstants.containerPadding),
               child: Column(
                 children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '질문하기',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontVariations: LayoutConstants.fontWeightBold),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(16)),
+                            minimumSize: Size(50, 25),
+                            backgroundColor: const Color(0xFFF1C40F),
+                            elevation: 2,
+                            alignment: Alignment.center),
+                        child: Text(
+                          '완료',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12,
+                              fontVariations: LayoutConstants.fontWeightBold),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        '누구에게 질문하고 싶나요?',
+                        style: TextStyle(color: Color(0xFF666666)),
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      DropdownButtonHideUnderline(
+                        child: DropdownButton<String>(
+                          value: searchval,
+                          onChanged: (String? newVal) {
+                            setState(() {
+                              searchval = newVal!;
+                            });
+                          },
+                          items: searchList
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                          style: TextStyle(color: Colors.black, fontSize: 14),
+                          dropdownColor: Colors.white,
+                          iconSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
                   ContainerCard(
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            Icon(Icons.radio_button_unchecked),
-                            SizedBox(
-                              width: 5,
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Test question 1',
-                                  style: TextStyle(
-                                      color: Colors.black, fontSize: 15),
-                                ),
-                                Text(
-                                  'who asked',
-                                  style: TextStyle(
-                                    color: Color(0xFF666666),
-                                    fontSize: 11,
-                                  ),
-                                )
-                              ],
-                            ),
-                            Spacer(
-                              flex: 1,
-                            ),
-                            TextButton(
-                              onPressed: () {},
-                              child: Text(
-                                '빠른 답변',
-                                style: TextStyle(
-                                    color: Color(0xFF666666), fontSize: 11),
+                      child: SizedBox(
+                    height: 150,
+                    child: TextField(
+                        maxLines: null,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            enabledBorder: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 5, horizontal: 0),
+                            hintText: '무엇을 물어보고 싶나요?'),
+                        style:
+                            const TextStyle(fontSize: 12, color: Colors.black)),
+                  )),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '질문 답변하기',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontVariations: LayoutConstants.fontWeightBold),
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 7),
+                        height: 30,
+                        width: 70,
+                        decoration: BoxDecoration(
+                            color: const Color(0xFFF1C40F),
+                            border: Border.all(
+                                color: const Color(0xFFF1C40F), width: 1),
+                            borderRadius: BorderRadius.circular(16)),
+                        child: DropdownButtonHideUnderline(
+                          child: DropdownButton<String>(
+                            value: selectedval,
+                            onChanged: (String? newVal) {
+                              setState(() {
+                                selectedval = newVal!;
+                              });
+                            },
+                            items: searchList
+                                .map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold),
+                            dropdownColor: Colors.white,
+                            iconSize: 20,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  ContainerCard(
+                    height: 220,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        //shrinkWrap: true,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.radio_button_unchecked),
+                              SizedBox(
+                                width: 5,
                               ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          children: [
-                            Icon(Icons.radio_button_unchecked),
-                            SizedBox(
-                              width: 5,
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Test question 1',
-                                  style: TextStyle(
-                                      color: Colors.black, fontSize: 15),
-                                ),
-                                Text(
-                                  'who asked',
-                                  style: TextStyle(
-                                    color: Color(0xFF666666),
-                                    fontSize: 11,
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    '첫 번째 질문',
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 15),
                                   ),
-                                )
-                              ],
-                            ),
-                            Spacer(
-                              flex: 1,
-                            ),
-                            TextButton(
-                              onPressed: () {},
-                              child: Text(
-                                '빠른 답변',
-                                style: TextStyle(
-                                    color: Color(0xFF666666), fontSize: 11),
+                                  Text(
+                                    '질문한 패밀리 이름',
+                                    style: TextStyle(
+                                      color: Color(0xFF666666),
+                                      fontSize: 11,
+                                    ),
+                                  )
+                                ],
                               ),
-                            ),
-                          ],
-                        ),
-                      ],
+                              // TextButton(
+                              //   onPressed: () {},
+                              //   child: Text(
+                              //     '빠른 답변',
+                              //     style: TextStyle(
+                              //         color: Color(0xFF666666), fontSize: 11),
+                              //   ),
+                              // ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Row(
+                            children: [
+                              Icon(Icons.radio_button_unchecked),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    '두 번째 질문',
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 15),
+                                  ),
+                                  Text(
+                                    '질문한 패밀리 이름',
+                                    style: TextStyle(
+                                      color: Color(0xFF666666),
+                                      fontSize: 11,
+                                    ),
+                                  )
+                                ],
+                              ),
+                              // TextButton(
+                              //   onPressed: () {},
+                              //   child: Text(
+                              //     '빠른 답변',
+                              //     style: TextStyle(
+                              //         color: Color(0xFF666666), fontSize: 11),
+                              //   ),
+                              // ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Row(
+                            children: [
+                              Icon(Icons.radio_button_unchecked),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    '세 번째 질문',
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 15),
+                                  ),
+                                  Text(
+                                    '질문한 패밀리 이름',
+                                    style: TextStyle(
+                                      color: Color(0xFF666666),
+                                      fontSize: 11,
+                                    ),
+                                  )
+                                ],
+                              ),
+                              // TextButton(
+                              //   onPressed: () {},
+                              //   child: Text(
+                              //     '빠른 답변',
+                              //     style: TextStyle(
+                              //         color: Color(0xFF666666), fontSize: 11),
+                              //   ),
+                              // ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                   SizedBox(
-                    height: 16,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(16)),
-                        minimumSize: Size(230, 45),
-                        backgroundColor: const Color(0xFFF1C40F),
-                        elevation: 2,
-                        alignment: Alignment.center),
-                    child: Text('질문 답변하러 가기',
-                        style: TextStyle(color: Colors.black, fontSize: 15)),
-                  ),
-                  SizedBox(
-                    height: 2,
-                  ),
-                  Text(
-                    '나에게 온 질문에 답변해보아요',
-                    style: TextStyle(color: Color(0xFF666666)),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(16)),
-                        minimumSize: Size(230, 45),
-                        backgroundColor: const Color(0xFFF1C40F),
-                        elevation: 2,
-                        alignment: Alignment.center),
-                    child: Text(
-                      '질문하러 가기',
-                      style: TextStyle(color: Colors.black, fontSize: 15),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 2,
-                  ),
-                  Text(
-                    '궁금한 점을 질문해보아요',
-                    style: TextStyle(color: Color(0xFF666666)),
+                    height: 15,
                   ),
                 ],
               ),
