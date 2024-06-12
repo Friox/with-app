fb0efe22ecd768b7980d6382000086ef93562b3d
Hanyeojun <hyj78396@naver.com>
2024-06-12 22:57:32 +0900

    일기 작성 페이지 생성

--- lib/screens/diary/diary_screen.dart
+++ lib/screens/diary/diary_screen.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:app/constants/layout_constants.dart';
 import 'package:app/models/diary_model.dart';
+import 'package:app/screens/diary/diary_write.dart';
 import 'package:app/screens/diary/widgets/diary_list_item.dart';
 import 'package:app/widgets/screen_header.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +18,8 @@ class TabBarDelegate extends SliverPersistentHeaderDelegate {
   const TabBarDelegate();
 
   @override
-  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
     return Container(
       color: Colors.white,
       child: TabBar(
@@ -35,21 +37,21 @@ class TabBarDelegate extends SliverPersistentHeaderDelegate {
       ),
     );
   }
-  
+
   @override
   double get maxExtent => 48;
-  
+
   @override
   double get minExtent => 48;
-  
+
   @override
   bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
     return false;
   }
 }
 
-class _DiaryScreenState extends State<DiaryScreen> with AutomaticKeepAliveClientMixin {
-
+class _DiaryScreenState extends State<DiaryScreen>
+    with AutomaticKeepAliveClientMixin {
   List<String> name = ["이승훈", "한여준", "이주환", "백승준", "최영준"];
 
   @override
@@ -58,75 +60,70 @@ class _DiaryScreenState extends State<DiaryScreen> with AutomaticKeepAliveClient
     return DefaultTabController(
       length: 2,
       child: NestedScrollView(
-        headerSliverBuilder: (context, innerBoxIsScrolled) {
-          return [
-            SliverToBoxAdapter(
-              child: ScreenHeader(
-                title: 'Diary',
-                subTitle: '오늘 일기를 작성하지 않았어요',
-                filledButton: FilledButton.icon(
-                  onPressed: () {
-
-                  },
-                  label: Text('작성'),
-                  icon: Icon(Icons.add),
-                  style: FilledButton.styleFrom(
-                    backgroundColor: Colors.green
-                  )
-                )
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              SliverToBoxAdapter(
+                child: ScreenHeader(
+                    title: 'Diary',
+                    subTitle: '오늘 일기를 작성하지 않았어요',
+                    filledButton: FilledButton.icon(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const DiaryWrite()));
+                        },
+                        label: Text('작성'),
+                        icon: Icon(Icons.add),
+                        style: FilledButton.styleFrom(
+                            backgroundColor: Colors.green))),
               ),
-            ),
-            SliverPersistentHeader(
-              pinned: true,
-              delegate: TabBarDelegate(),
-            )
-          ];
-        },
-        body: TabBarView(
-          physics: NeverScrollableScrollPhysics(),
-          children: [
-            CustomScrollView(
-              key: PageStorageKey<String>('tab1'),
-              slivers: [
+              SliverPersistentHeader(
+                pinned: true,
+                delegate: TabBarDelegate(),
+              )
+            ];
+          },
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: [
+              CustomScrollView(key: PageStorageKey<String>('tab1'), slivers: [
                 SliverPadding(
-                  padding: EdgeInsets.only(bottom: LayoutConstants.headerPaddingVertical),
+                  padding: EdgeInsets.only(
+                      bottom: LayoutConstants.headerPaddingVertical),
                   sliver: SliverList(
                     delegate: SliverChildBuilderDelegate(
-                      (context, index) => DiaryListItem(data: Diary(
-                        date: DateTime.now().subtract(Duration(days: index)),
-                        writer: name[Random().nextInt(name.length - 1)],
-                        sentiment: index % 6
-                      )),
-                      childCount: 20
-                    ),
+                        (context, index) => DiaryListItem(
+                            data: Diary(
+                                date: DateTime.now()
+                                    .subtract(Duration(days: index)),
+                                writer: name[Random().nextInt(name.length - 1)],
+                                sentiment: index % 6)),
+                        childCount: 20),
                   ),
                 )
-              ]
-            ),
-            CustomScrollView(
-              key: PageStorageKey<String>('tab2'),
-              slivers: [
+              ]),
+              CustomScrollView(key: PageStorageKey<String>('tab2'), slivers: [
                 SliverPadding(
-                  padding: EdgeInsets.only(bottom: LayoutConstants.headerPaddingVertical),
+                  padding: EdgeInsets.only(
+                      bottom: LayoutConstants.headerPaddingVertical),
                   sliver: SliverList(
                     delegate: SliverChildBuilderDelegate(
-                      (context, index) => DiaryListItem(data: Diary(
-                        date: DateTime.now().subtract(Duration(days: index)),
-                        writer: name[Random().nextInt(name.length - 1)],
-                        sentiment: index % 6
-                      )),
-                      childCount: 20
-                    ),
+                        (context, index) => DiaryListItem(
+                            data: Diary(
+                                date: DateTime.now()
+                                    .subtract(Duration(days: index)),
+                                writer: name[Random().nextInt(name.length - 1)],
+                                sentiment: index % 6)),
+                        childCount: 20),
                   ),
                 )
-              ]
-            ),
-          ],
-        )
-      ),
+              ]),
+            ],
+          )),
     );
   }
-  
+
   @override
   bool get wantKeepAlive => true;
 }
